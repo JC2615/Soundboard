6076c24497b8c840cb0b6a73afa0b347cb9efbb9
Josh Curry <joshuacurry2615@gmail.com>
2020-06-14 21:21:40 -0400

    First complete build

--- src/apis/audio_api.js
+++ src/apis/audio_api.js
@@ -2,14 +2,68 @@ import { Audio } from "expo-av";
 
 const soundObject = new Audio.Sound();
 
+const bark = require("../../assets/bark.mp3");
+const bleat = require("../../assets/bleat.mp3");
+const buzz = require("../../assets/buzz.mp3");
+const meow = require("../../assets/meow.mp3");
+const neigh = require("../../assets/neigh.mp3");
+const moo = require("../../assets/moo.mp3");
+const croak = require("../../assets/croak.mp3");
+const chirp = require("../../assets/chirp.mp3");
+const blub = require("../../assets/blub.mp3");
+const hiss = require("../../assets/hiss.mp3");
+
 export async function playSound(soundName) {
   await soundObject.unloadAsync();
-      try {
-        await soundObject.loadAsync(require("../../assets/bark.mp3"));
-        await soundObject.playAsync();
-        // Your sound is playing!
-      } catch (error) {
-        // An error occurred!
-        await soundObject.playAsync();
-      }
+  try {
+    switch (soundName) {
+      case "bark":
+        await soundObject.loadAsync(bark);
+        break;
+
+      case "bleat":
+        await soundObject.loadAsync(bleat);
+        break;
+
+      case "blub":
+        await soundObject.loadAsync(blub);
+        break;
+
+      case "buzz":
+        await soundObject.loadAsync(buzz);
+        break;
+
+      case "chirp":
+        await soundObject.loadAsync(chirp);
+        break;
+
+      case "croak":
+        await soundObject.loadAsync(croak);
+        break;
+
+      case "hiss":
+        await soundObject.loadAsync(hiss);
+        break;
+
+      case "meow":
+        await soundObject.loadAsync(meow);
+        break;
+
+      case "moo":
+        await soundObject.loadAsync(moo);
+        break;
+
+      case "neigh":
+        await soundObject.loadAsync(neigh);
+        break;
+
+      default:
+        break;
+    }
+    await soundObject.playAsync();
+    // Your sound is playing!
+  } catch (error) {
+    // An error occurred!
+    await soundObject.playAsync();
+  }
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,39 +1,46 @@
 import React, {useState} from "react";
-import { View, Text, StyleSheet, Alert, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, Alert, TouchableOpacity, ScrollView} from "react-native";
 import { playSound } from "../apis/audio_api";
 import SortableGridView from "react-native-sortable-gridview";
 import { SimpleLineIcons } from '@expo/vector-icons';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-
+function chooseName(nameList, state) {
+  let namesUsed = [];
+  for (const soundObject in state) {
+    namesUsed.push(state[soundObject].name);
+  }
+  let randomElement = nameList[Math.floor(Math.random() * nameList.length)];
+  while(namesUsed.includes(randomElement)){
+    randomElement = nameList[Math.floor(Math.random() * nameList.length)];
+  }
+  return randomElement;
+}
 
 function HomeScreen() {
-  const [sounds, setSounds] = useState([
-    { name: "Bark", backgroundColor: "#09f", color: "#fff" },
-        { name: "Meow", backgroundColor: "#f60", color: "#fff" },
-        { name: "Bleat", backgroundColor: "#333", color: "#fff" },
-        {
-          name: "Recording",
-          backgroundColor: "#rgba(255, 216, 58, 1)",
-          color: "#333",
-        },
-  ]);
-
+  const [sounds, setSounds] = useState([]);
+  let lockData = [];
+  const soundNames = ["Bark", "Meow", "Bleat", "Chirp", "Blub", "Neigh", "Buzz", "Croak", "Hiss", "Moo"];
+  if (sounds.length < 10) {
+    lockData.push({
+      name: 'Add box',
+    })
+  }
   return (
-    <View>
+    <ScrollView>
       <SortableGridView
       data={sounds}
+      lockData={lockData}
+      aspectRatio={1}
       sensitivity={150} 
-      onDragStart={() => {
-        console.log("Default onDragStart");
-      }}
-      onDragRelease={(data) => {
-        //console.log("Default onDragRelease", data);
-        //setSounds(sounds);
-      }}
+      selectAnimation="shake"
+      numPerRow={2}
+      onDragStart={() => {}}
+      onDragRelease={(data) => {setSounds(sounds)}}
       renderItem={(item, index) => {
         return (
           <View
-            uniqueKey={item.name} // Important! Should add this props!!!
+            uniqueKey={item.backgroundColor} // Important! Should add this props!!!
             onTap={() => playSound(item.name.toLowerCase())}
             style={[styles.item, { backgroundColor: item.backgroundColor }]}
           >
@@ -50,17 +57,58 @@ function HomeScreen() {
           <TouchableOpacity
             style={styles.cover}
             onPress={() => {
-              let data = [...sounds];
+              Alert.alert(
+                "Delete Sound",
+                "Tap Delete to delete this sound",
+                [
+                  {text: "Cancel"},
+                  {text: "Delete", onPress: () => {
+                    let data = [...sounds];
               data.splice(index, 1);
               setSounds(data);
+                  } },
+                ]
+              )
+              
             }}
           >
             <SimpleLineIcons name="trash" size={24} color="gray" />
           </TouchableOpacity>
         )
       }}
+
+      renderLockItem={(item, index) => {
+        return (
+          <View
+            uniqueKey={index}
+            style={styles.lockItem}
+            onTap={() => {
+              Alert.alert(
+                "Add Sound",
+                "Tap OK to add a new sound",
+                [
+                  {text: "Cancel"},
+                  {text: "OK", onPress: () => {
+                    let data = [...sounds];
+              const randomColor = `#rgba(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, 1)`;
+              const name = chooseName(soundNames, sounds);
+              data.push({
+                name,
+                backgroundColor: randomColor,
+                color: '#fff'
+              });
+              setSounds(data);
+                  }}
+                ]
+              )
+            }}
+          >
+            <Text>{item.name} +</Text>
+          </View>
+        )
+      }}
     />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -73,6 +121,13 @@ const styles = StyleSheet.create({
   },
   cover: {
     backgroundColor: "transparent",
+  },
+  lockItem: {
+    flex: 1,
+    backgroundColor: "gray",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
 
